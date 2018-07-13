2aaee4a802fddcbc45e219325afac6dc3d64b5b1
Corin Lawson <corin@responsight.com>
2018-07-13 10:05:01 +1000

    Fine tune exports
    This change makes the main exports explicit and removes the erroneous export of grid.
    
    Also, bump the d3-selection version.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-export * from './src/axes';
+export {
+  axes,
+  axesPositionBottom, axesPositionLeft,
+  axesPositionTop, axesPositionRight,
+  axesPositionDefault, axesPosition
+} from './src/axes';
 export { grid as axesGrid } from './src/grid';
