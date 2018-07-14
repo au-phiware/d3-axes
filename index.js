802984696d7a3f89fcf0e29ca1fdc720a5965d62
Corin Lawson <corin@phiware.com.au>
2018-07-14 14:50:40 +1000

    Demo axesShape
    The new axesShape function encapsulates the General Update Pattern for a
    path generator and it also handles scaling to the axes' scales. Adding a
    curve to the plot is now greatly simplified to a call to axesShape. E.g.
    
        wrap(axes(...), axesShape(line()).data(...))
    
    An intermediary call to plot is added to render the curve before the
    animation, so that the curve is also animated.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { axisBottom, axisLeft } from 'd3-axis';
 import { csv } from 'd3-fetch';
 import { extent } from "d3-array";
 import { line } from "d3-shape";
-import { axes, axesPositionBottom, axesGrid } from 'd3-axes';
+import { axes, axesPositionBottom, axesGrid, axesShape as shape } from 'd3-axes';
 import { compose } from 'd3-compose';
 import { wrap } from 'd3-wrap';
 
@@ -27,12 +27,13 @@ let x = d => +d.t
   , y = d => +d.v
   , xAxis = axisBottom().scale(scaleLinear())
   , yAxis = axisLeft().scale(scaleLinear())
-  , plot = axes(
+  , plot = wrap(
+      axes(
         compose(axesPositionBottom, xAxis),
         compose(
           function(selection) {
             let label = selection.select('.label');
-            
+
             if (!label.size()) {
               label = selection.append('text')
                 .attr('class', 'label');
@@ -56,10 +57,9 @@ let x = d => +d.t
       .padding(20, 20, 30, 50)
       .width(document.documentElement.clientWidth)
       .height(500)
-      .domain([0,0.1], [-0.01,0.01])
-  , curve = line()
-    .x(d => plot.x().scale()(x(d)))
-    .y(d => plot.y().scale()(y(d)));
+      .domain([0.01,0.03], [-0.002,0.002])
+    , shape(line().x(x).y(y))
+    );
 
 
 let svg = d3.select("svg")
@@ -69,22 +69,13 @@ let svg = d3.select("svg")
 plot(svg);
 
 csv("data.csv").then(data => {
+  plot.data([data]);
+  plot(svg);
   plot.domain(
     extent(data, x),
     extent(data, y)
   );
 
-  let curves = svg.transition().duration(900)
-      .call(plot)
-      .select('.axes')
-      .selectAll('.curve').selection()
-      .data([data]);
-
-  curves.exit().remove();
-
-  curves.enter()
-      .append('path')
-      .attr('class', 'curve')
-    .merge(curves)
-      .attr('d', curve);
+  svg.transition().duration(900)
+      .call(plot);
 });
